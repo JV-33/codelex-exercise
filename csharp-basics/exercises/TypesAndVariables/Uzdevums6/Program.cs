143784f1777f1ea24bcd8ad976a890318c4a17a8
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-07-25 07:50:30 +0300

    Ex7

--- csharp-basics/exercises/TypesAndVariables/Uzdevums6/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Uzdevums6/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Uzdevums6;
+﻿using System;
+using System.Linq;
 
-class Program
+namespace Uzdevums6
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ieraksti skaitli");
+            string user_input = Console.ReadLine();
+            int sum = user_input.Where(char.IsDigit).Sum(c => c - '0');
+
+            Console.WriteLine($"Visu skaitļu summa: {sum}");
+
+            Console.ReadKey();
+        }
     }
 }
-
