7647151a2105a324dba90c1bcd789d7826e165bb
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-07-25 08:08:09 +0300

    ex7

--- csharp-basics/exercises/TypesAndVariables/ex7/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/ex7/Program.cs
@@ -4,7 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Ieraksti kādu tekstu lai uzzinātu no cik skaitļiem tas sastāv");
+        
+        string userInput = Console.ReadLine();
+
+        int beginingNumber = 0;
+
+        foreach (char c in userInput)
+        {
+            beginingNumber++;
+        }
+
+
+        Console.WriteLine($"Jūsu uzrakstītajā ir {beginingNumber}");
+
+        Console.ReadKey();
     }
 }
 
