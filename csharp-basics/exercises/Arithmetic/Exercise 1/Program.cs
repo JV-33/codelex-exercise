5e5e0f0a3292fa6de239f502aadfe75707e210b4
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-07-26 21:54:08 +0300

    SumAverageRunningint

--- csharp-basics/exercises/Arithmetic/Exercise 1/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise 1/Program.cs
@@ -1,45 +1,25 @@
-﻿namespace Exercise_1;
-
-class Program
+﻿namespace Exercise_1
 {
-    static void Main(string[] args)
+    class Program
     {
-        int first = 2;
-        int second = 5;
-
-        int sum = first + second;
-        int div = first - second;
-
-
-
-
-
-        if (first == 15 || second == 15)
+        static void Main(string[] args)
         {
-            Console.WriteLine("true");
+            int first = 2;
+            int second = 5;
+
+            int sum = first + second;
+            int div = Math.Abs(first - second);
+
+            if (first == 15 || second == 15 || sum == 15 || div == 15)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.ReadKey();
         }
-
-        else if (sum == 15) 
-        {
-            Console.WriteLine("true");
-        }
-
-         else if (div == 15) 
-        {
-            Console.WriteLine("true");
-        }
-
-        else
-        {
-            Console.WriteLine("false");
-        }
-
-
-            
-
-
-       
-        Console.ReadKey();
     }
 }
-
---
+++ csharp-basics/exercises/Arithmetic/Exercise 2/CheckOddEven.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_2
+{
+    class CheckOddEven
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("bye!");
+        }
+    }
+}
