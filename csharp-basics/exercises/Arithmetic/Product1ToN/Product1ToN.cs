9e30777a15a2ff5128b80b0ce62cd97159d26762
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-07-26 22:08:25 +0300

    Product1ToN

---
+++ csharp-basics/exercises/Arithmetic/Product1ToN/Product1ToN.cs
@@ -0,0 +1,20 @@
+﻿namespace Product1ToN;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sākums = 1;
+        
+
+        
+
+        for (int i = 1; i <= 10; i++)
+        {
+            sākums *= i;
+        }
+
+        Console.WriteLine($"Skaitļu reizinājums no 1 līdz 10 ir {sākums}");
+    }
+}
+
