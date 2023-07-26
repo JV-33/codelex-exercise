5e3145a3f43aa329f5b213e995a2f738d3e0305a
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-07-26 21:00:54 +0300

    Exercise 1

---
+++ csharp-basics/exercises/Arithmetic/Exercise 1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exercise_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int first = 2;
+        int second = 5;
+
+        int sum = first + second;
+        int div = first - second;
+
+
+
+
+
+        if (first == 15 || second == 15)
+        {
+            Console.WriteLine("true");
+        }
+
+        else if (sum == 15) 
+        {
+            Console.WriteLine("true");
+        }
+
+         else if (div == 15) 
+        {
+            Console.WriteLine("true");
+        }
+
+        else
+        {
+            Console.WriteLine("false");
+        }
+
+
+            
+
+
+       
+        Console.ReadKey();
+    }
+}
+
