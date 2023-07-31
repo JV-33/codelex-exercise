e0bd78948ea08d57f60ba961d50057c02c86f768
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-07-31 15:57:18 +0300

    Pielabots PhoneKeyPad

--- csharp-basics/exercises/FlowOfControl/PhoneKeyPad/Program.cs
+++ csharp-basics/exercises/FlowOfControl/PhoneKeyPad/Program.cs
@@ -25,8 +25,65 @@ class Program
             else result.Append(v);
         }
         Console.WriteLine($"Iznākums {result}");
-        Console.ReadLine();
 
+        Console.WriteLine("Uzraksti tekstu ko vēlies uzzināt ciparu sistēmā :");
+
+        var input1 = Console.ReadLine().ToUpper();
+        StringBuilder result1 = new StringBuilder();
+
+        foreach (char c in input1)
+        {
+            switch (c)
+            {
+                case 'A':
+                case 'B':
+                case 'C':
+                    result1.Append('2');
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    result1.Append('3');
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    result1.Append('4');
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    result1.Append('5');
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    result1.Append('6');
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    result1.Append('7');
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    result1.Append('8');
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    result1.Append('9');
+                    break;
+                default:
+                    result1.Append(c);
+                    break;
+            }
+        }
+        Console.WriteLine("Rezultāts: " + result1.ToString());
+        Console.ReadLine();
 
 
 
