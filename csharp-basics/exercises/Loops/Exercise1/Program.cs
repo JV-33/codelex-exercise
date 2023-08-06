2f49e286b7daee6f94ab02eddfbfc47c1c570866
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-08-06 15:09:33 +0300

    Exercise 1

--- csharp-basics/exercises/Loops/Exercise1/Program.cs
+++ csharp-basics/exercises/Loops/Exercise1/Program.cs
@@ -6,12 +6,16 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            int i;
+            //int i = 0  ;
 
             Console.WriteLine("The first 10 natural numbers are: ");
-
+            
             //todo - write a program in C# to display the first 10 natural numbers
-
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
         }
     }
 }
