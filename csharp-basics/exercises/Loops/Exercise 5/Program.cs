4c11370f6c2e5293a1c22e46a588593a37253698
Janis Virsnitis <virsnitisjanis@icloud.com>
2023-08-07 17:49:38 +0300

    Exercise 5

---
+++ csharp-basics/exercises/Loops/Exercise 5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Uzraksti pirmo vārdus!");
+
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Uzraksti otro vārdus!");
+
+        string input1 = Console.ReadLine();
+
+        string result = input;
+
+        int kopaZimes = 30;
+        int tagadejaisGarums = result.Length + input1.Length + 1;
+        int punktuDaudzums = kopaZimes - tagadejaisGarums;
+
+
+        for (int i = 0; i < punktuDaudzums; i++)
+        {
+            result += ".";
+        }
+
+        result += input1;
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
+
