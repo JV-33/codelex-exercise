671c95a2f9c8545a411a2f15fd218e9786129397
JV-33 <virsnitisjanis@icloud.com>
2023-08-12 15:34:45 +0300

    Izmaiņas 4 Exercise

--- csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movies.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movies.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 namespace Exercise_4
 {
-	public class Movies
-	{
-		public Movies()
-		{
-		}
-	}
-}
+    public class Movies
+    {
+        private string _title;
+        private string _studio;
+        private string _raiting;
 
+        public string Title { get { return _title; } }
+        public string Studio { get { return _studio; } }
+        public string Rating { get { return _raiting; } }
+
+
+
+        public Movies(string title, string studio, string raiting)
+        {
+            _title = title;
+            _studio = studio;
+            _raiting = raiting;
+        }
+
+        public Movies(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _raiting = "PG";
+        }
+
+        public static Movies[] GetPG(Movies[] movies)
+        {
+            int count = 0;
+            foreach (var movie in movies)
+            {
+                if (movie._raiting == "PG")
+                {
+                    count++;
+                }
+            }
+            Movies[] pgMovies = new Movies[count];
+
+            int index = 0;
+            foreach (var movie in movies)
+            {
+                if (movie._raiting == "PG")
+                {
+                     pgMovies[index] = movie;
+                     index++;
+                }
+            }
+
+                return pgMovies;
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
@@ -4,7 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Movies casinoRojal = new Movies("Casino Royale", "Eon Productions", "PG­13");
+        Movies glass = new Movies("Glass", "Buena Vista International", "PG­13");
+        Movies spidy = new Movies("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+        Console.WriteLine($"Title: {casinoRojal.Title}, Studio: {casinoRojal.Studio}, Rating: {casinoRojal.Rating}");
+        Console.WriteLine($"Title: {glass.Title}, Studio: {glass.Studio}, Rating: {glass.Rating}");
+        Console.WriteLine($"Title: {spidy.Title}, Studio: {spidy.Studio}, Rating: {spidy.Rating}");
+        Console.ReadKey();
+        
     }
 }
 
