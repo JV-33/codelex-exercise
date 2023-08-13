ba9f8034b5ea8538b8a0c523523b7486de4147db
JV-33 <virsnitisjanis@icloud.com>
2023-08-13 08:48:11 +0300

    Exercise 8

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+            SwapPoints(ref p1, ref p2);
+            Console.WriteLine("(" + p1.X + ", " + p1.Y + ")");
+            Console.WriteLine("(" + p2.X + ", " + p2.Y + ")");
+            Console.ReadKey();
+        }
+
+        static void SwapPoints(ref Point p1, ref Point p2)
+        {
+            Point temp = p1;
+            p1 = p2;
+            p2 = temp;
+        }
+    }
+}
