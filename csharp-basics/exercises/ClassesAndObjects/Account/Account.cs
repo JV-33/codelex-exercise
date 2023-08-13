23a4dce972cb17994f0a7b6244e2877e52e3c49c
JV-33 <virsnitisjanis@icloud.com>
2023-08-13 21:23:57 +0300

    Account

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -1,7 +1,58 @@
-﻿namespace Account
+﻿using System;
+
+namespace Account
 {
     class Account
     {
+        private double _balance;
+        private string _name;
+        
+        
+
+        public double Balance  => _balance;
+        public string Name => _name; 
+        
+
+        public  Account(string name, double balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public void Withdrawal(double withdrawal)
+        {
+            if (_balance >= withdrawal)
+            {
+                _balance -= withdrawal;
+            }
+            else
+            {
+                Console.WriteLine("Kontā nepietiek līdzekļi");
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{_name}: {_balance}";
+        }
+
+
+        public void Deposit(double deposit)
+        {
+            _balance += deposit;
+        }
+
+        public static void Transfer(Account from, Account to, double howMuch)
+        {
+            from.Withdrawal(howMuch);
+            to.Deposit(howMuch);
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
         
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace Account
+﻿using System;
+
+namespace Account
 {
     class Program
     {
         private static void Main(string[] args)
         {
+            Account firstAccount = new Account("My first account", 100.00);
+            firstAccount.Deposit(20);
+            Console.WriteLine(firstAccount);
+
+            Account matt = new Account("Matt's account", 1000.00);
+            matt.Withdrawal(100.00);
+            Account myAccount = new Account("My account", 0.00);
+            myAccount.Deposit(100.00);
+            Console.WriteLine(matt);
+            Console.WriteLine(myAccount);
+
+            Account A = new Account("A account", 100.00);
+            Account B = new Account("B account", 0.00);
+            Account C = new Account("C account", 0.00);
+
+            Account.Transfer(A, B, 50.00);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+
+            Account.Transfer(B, C, 25.00);
+            Console.WriteLine(B);
+            Console.WriteLine(C);
+
+            Console.ReadKey();
+
+
         }
     }
 }
