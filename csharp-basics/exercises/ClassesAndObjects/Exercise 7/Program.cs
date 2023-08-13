909903524dd3b7a2d053e91b1fb9bf9b57f328e4
JV-33 <virsnitisjanis@icloud.com>
2023-08-13 08:30:28 +0300

    Exercise 7

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Exercise_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("How much money is in the account?: ");
+        double startingBalance = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the annual interest rate:");
+        double annualInterestRate = Convert.ToDouble(Console.ReadLine()) / 100;
+
+        SavingsAccount account = new SavingsAccount(startingBalance)
+        {
+            AnnualInterestRate = annualInterestRate
+        };
+
+        Console.Write("How long has the account been opened? ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= months; i++)
+        {
+            Console.Write($"Enter amount deposited for month: {i}: ");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            account.Deposit(deposit);
+
+            Console.Write($"Enter amount withdrawn for {i}: ");
+            double withdrawal = Convert.ToDouble(Console.ReadLine());
+            account.Withdraw(withdrawal);
+
+            account.AddMonthlyInterest();
+        }
+
+        Console.WriteLine($"Total deposited: ${account.TotalDeposits:0.00}");
+        Console.WriteLine($"Total withdrawn: ${account.TotalWithdrawals:0.00}");
+        Console.WriteLine($"Interest earned: ${account.TotalInterestEarned:0.00}");
+        Console.WriteLine($"Ending balance: ${account.Balance:0.00}");
+        Console.ReadKey();
+    }
+}
+
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/SavingsAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Exercise_7
+{
+    public class SavingsAccount
+    {
+        private double _annualInterestRate;
+        private double _balance;
+        private double _totalDeposits = 0;
+        private double _totalWithdrawals = 0;
+        private double _totalInterestEarned = 0;
+
+        public SavingsAccount(double startingBalance)
+        {
+            _balance = startingBalance;
+        }
+
+        public double AnnualInterestRate
+        {
+            get { return _annualInterestRate; }
+            set { _annualInterestRate = value; }
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+            _totalDeposits += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (_balance >= amount)
+            {
+                _balance -= amount;
+                _totalWithdrawals += amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+        }
+
+        public void AddMonthlyInterest()
+        {
+            double monthlyInterest = (_balance * _annualInterestRate) / 12;
+            _balance += monthlyInterest;
+            _totalInterestEarned += monthlyInterest;
+        }
+
+        public double TotalDeposits => _totalDeposits;
+        public double TotalWithdrawals => _totalWithdrawals;
+        public double TotalInterestEarned => _totalInterestEarned;
+        public double Balance => _balance;
+    }
+}
