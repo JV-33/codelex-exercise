dd3255531ae064a853cc163b685cdc7547374235
JV-33 <virsnitisjanis@icloud.com>
2023-08-14 10:55:30 +0300

    Exercise 14

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 14/Date.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_14
+{
+	public class Date
+	{
+        private const string DutchCultureCode = "nl-NL";
+
+        /// <summary>
+        /// Returns the name of a weekday in Dutch for a given date.
+        /// </summary>
+
+        public static  string GetCurantData(int year, int month, int day)
+		{
+            DateTime dt = new DateTime(year, month, day);
+            CultureInfo dutchCulture = CultureInfo.GetCultureInfo(DutchCultureCode);
+            return dutchCulture.DateTimeFormat.GetDayName(dt.DayOfWeek).ToLower();
+        }
+	}
+}
+
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 14/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise_14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Date.GetCurantData(1970, 9, 21));
+        Console.WriteLine(Date.GetCurantData(1945, 9, 2));   
+        Console.WriteLine(Date.GetCurantData(2001, 9, 11));
+        Console.ReadKey();
+    }
+}
+
