a36b59080047903ce3d0d3a8a9d8f111879baa8f
JV-33 <virsnitisjanis@icloud.com>
2023-08-21 16:25:53 +0300

    Exercise3

---
+++ csharp-basics/exercises/Polymorphism/Exercise3/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Exercise3
+{
+	public class Student : Person
+	{
+		private double Gpa { get; set; }
+
+		public Student(double gpa)
+		{
+			Gpa = gpa;
+		}
+
+		public override void Display()
+		{
+			base.Display();
+			Console.WriteLine($"Nav ne jausmas, kas ir : {Gpa}");
+		}
+	}
+}
+
